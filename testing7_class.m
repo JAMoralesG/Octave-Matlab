ffc0660071720adf508755cedf91cf8b157afd21
JAMoralesG <39317777+JAMoralesG@users.noreply.github.com>
2019-01-28 11:14:03 -0600

    Add files via upload

---
+++ testing7_class.m
@@ -0,0 +1,10 @@
+A=rand(3)
+limit=0.5;
+[B]=(A>limit)
+[i j]=find(B);
+if sum(sum(B))>0
+  fprintf('Indices de valores > %4.2f: \n',limit)
+  disp([i j])
+ else 
+  disp('Todos los valores son menores que el limite')
+endif
