b43fadf4f168ae5019fc010d6b90ca4fbdac8eaf
JAMoralesG <39317777+JAMoralesG@users.noreply.github.com>
2019-01-28 11:51:58 -0600

    Add files via upload

---
+++ testing8_class.m
@@ -0,0 +1,19 @@
+##numero=input('Ingresa un numero:');
+##switch numero 
+##   case -1
+##     disp('Menos uno');
+##   case 0
+##     disp('cero');
+##   case 1
+##     disp('Uno');
+##   otherwise
+##     disp('otro valor');
+##endswitch
+n=1;
+nFact=1;
+while nFact< 1e10
+  n=n+1;
+  nFact=nFact*n;
+endwhile
+fprintf('n: %d\n',n);
+fprintf('n!: %d\n',nFact);
---
+++ testing9_class.m
@@ -0,0 +1,7 @@
+n=5;
+hilbert_mat=zeros(n);
+for r=n:-1:1
+  for c=n:-1:1
+    hilbert_mat(r,c)=1/(r+c-1)
+  endfor
+endfor
